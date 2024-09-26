944daf0ad896f0977fffd2fed855e58a55d6ebad
ryand123456789 <157507067+ryand123456789@users.noreply.github.com>
2024-09-26 12:07:05 -0400

    nnn
    n nnn

---
+++ ambrose.lua
@@ -0,0 +1,13 @@
+gg.alert("Hello beta","Contunie") --essentially the introduction
+gg.toast("I am him btw")
+
+if gg.isVisible(true) then gg.setVisibility(false) end 
+
+home = gg.choice({
+    "Option 1 Tom"
+    "Option 2 Scal"
+    "Option 3 Ambrose"
+    "Option 4 egg"
+    "Option 5 winner"
+    "Exit"},nil,"Choose Option"
+})
