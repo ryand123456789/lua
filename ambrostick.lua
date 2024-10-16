99694db5d75fd78bd8a9a12786997b54c1ed9516
ryand123456789 <157507067+ryand123456789@users.noreply.github.com>
2024-10-16 12:09:26 -0400

    Update ambrostick.lua

--- ambrostick.lua
+++ ambrostick.lua
@@ -1,4 +1,4 @@
-gg.alert("WELCOME TO THE: Tom P ModMenu","Continue") --essentially the introduction
+gg.alert("Stickman and Gun Mod Menu","Continue") --essentially the introduction
 
 if gg.isVisible(true) 
 then 
@@ -6,26 +6,32 @@ then
 end 
 
 HOME = gg.choice({
-    "Coins Generate",
-    "Gems Generate",
-    "Free Container in Shop Section"},
-     nil, "Hello Mr P.")
+    "Coins Generate", --HOME==1
+    "Gems Generate",--HOME==2
+    "Infinite Ammo",--HOME==3
+    "PLACEHOLDER",--HOME==4
+    "PLACEHOLDER",--HOME==1
+    "PLACEHOLDER"--HOME==5
+},
+     nil, "Hello Mr P. Select what option you want")
 if HOME == 1
 then 
-    prompt = gg.prompt({"Type the amount of coins you have currently: (WARNING: if your coin value is near 0 the game may break or not change your coin value)"},nil,{[1]='number'})
+    prompt = gg.prompt({"Type the amount of coins you have currently: (WARNING: if your coin value is low the game may break or not change your coin value)"},nil,{[1]='number'})
     gg.searchNumber(prompt[1],gg.TYPE_DWORD)
     gg.getResults(100)
     prompt = gg.prompt({"Type the amount of coins want:"},nil,{[2]='number'})
+    prompt = "EDIT DECRYPTOR VALUE"
     gg.editAll(prompt[1],gg.TYPE_DWORD)
     gg.clearResults(100)
     gg.alert("Done! Please buy something to show the currency on the top left of your screen","OK")
 end
 if HOME == 2
 then 
-    prompt = gg.prompt({"Type the amount of gems you have currently: (WARNING: if your gem value is near 0 the game may break or not change your gem value)"},nil,{[1]='number'})
+    prompt = gg.prompt({"Type the amount of gems you have currently: (WARNING: if your gem value is low the game may break or not change your gem value)"},nil,{[1]='number'})
     gg.searchNumber(prompt[1],gg.TYPE_DWORD)
     gg.getResults(100)
-    prompt = gg.prompt({"Type the amount of gems want:"},nil,{[2]='number'})
+    prompt = gg.prompt({"Type the amount of gems you want:"},nil,{[2]='number'})
+    prompt = "EDIT DECRYPTOR VALUE"
     gg.editAll(prompt[1],gg.TYPE_DWORD)
     gg.clearResults(100)
     gg.alert("Done! Please buy something to show the currency on the top left of your screen","OK")
@@ -40,6 +46,10 @@ then
     gg.clearResults(100)
 end
     --search for DWORD value (space in mem)
+
+
+
+    
 
 
 
